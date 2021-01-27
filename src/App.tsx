333e9d336ee6ead128a460e0afbd4c60b84f1f18
ericsonmoreira <moreira.ericson@gmail.com>
2021-01-27 11:07:35 -0300

    eslint anda editor config

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
-import { ChakraProvider, Text, theme } from "@chakra-ui/react";
-import * as React from "react";
+import { ChakraProvider, Text, theme } from '@chakra-ui/react';
+import * as React from 'react';
 
 export const App = () => (
   <ChakraProvider theme={theme}>
